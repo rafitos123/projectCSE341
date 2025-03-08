cea0b3ff38afcc0bd76c6390352f07fb933bac7c
lolrafael123 <rafael.antunes2705@gmail.com>
2025-03-08 15:41:54 -0300

    project

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+/*router.get('/', (req, res) => {
+    res.send('Hello, world!');
+    });
+*/
+
+
+router.use('/users', require('./users'));
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const mongodb = require('./db/database');
+
+const port = process.env.PORT || 3000;
+
+app.use('/', require('./routes'));
+
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => {console.log(`Databse is listening and node is running on port ${port}`);});
+    }
+});
+
