a7c2a11d599f78699047117fdf4fb0a3bd3cabbc
lolrafael123 <rafael.antunes2705@gmail.com>
2025-03-08 16:08:00 -0300

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
-/*router.get('/', (req, res) => {
+router.get('/', (req, res) => {
     res.send('Hello, world!');
     });
-*/
+
 
 
 router.use('/users', require('./users'));
