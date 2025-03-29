222e1fb3b246ed522eb36f95f0cd7f2559ba51af
lolrafael123 <rafael.antunes2705@gmail.com>
2025-03-29 19:21:00 -0300

    oatuh

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.use('/soccer', require('./soccer'));
 router.use('/club', require('./club'));
 
 router.get('/login', passport.authenticate('github'), (req, res) => {});
-router.get('logout', (req, res, next) => {
+router.get('/logout', (req, res, next) => {
     req.logout(function(err) {
         if (err) { return next(err); }
         res.redirect('/');
