2ac6b983895cb4369e2e84514891995a26da2fc5
lolrafael123 <rafael.antunes2705@gmail.com>
2025-03-29 19:42:41 -0300

    changes

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const passport = require('passport');
 const session = require('express-session');
 const GitHubStrategy = require('passport-github2').Strategy;
 const cors = require('cors');
+const MongoStore = require('connect-mongo');
 
 const port = process.env.PORT || 3000;
 
@@ -17,6 +18,10 @@ app
         secret: "secret",
         resave: false,
         saveUninitialized: true,
+        store: MongoStore.create({
+            mongoUrl: process.env.MONGO_URI, // URL do MongoDB
+            collectionName: 'sessions'
+        })
     }))
     .use(passport.initialize())
     .use(passport.session())
