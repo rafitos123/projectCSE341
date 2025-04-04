928b13c35acccbf8a74ac64870ae89a067a3b88d
lolrafael123 <rafael.antunes2705@gmail.com>
2025-04-04 20:25:50 -0300

    changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const session = require('express-session');
 const GitHubStrategy = require('passport-github2').Strategy;
 const cors = require('cors');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 
 app
